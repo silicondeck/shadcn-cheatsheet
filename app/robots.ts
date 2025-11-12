66c93f1d2c741330ad24fd8cc9cdbb7118993152
monab <monajmakwana@gmail.com>
2025-11-12 18:23:32 +0530

    feat(sitemap): added sitemap and robots files

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://shadcnstore.com/cheatsheet"
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/", "/static/"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  }
+}
